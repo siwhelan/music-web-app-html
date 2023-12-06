1f95bb7b8ebf0b14b72b8dfceae3b9674362d977
siwhelan <simon@swhelan.dev>
2023-12-06 11:47:31 +0000

    pre-challenge file tidy

--- example_routes.py
+++ example_routes.py
@@ -3,19 +3,20 @@ from lib.book_repository import BookRepository
 from lib.book import Book
 from flask import request, render_template, redirect, url_for
 
+
 # You won't need to nest your routes in app.py in a method like this
 def apply_example_routes(app):
     # GET /books
     # Returns a list of books
-    @app.route('/books', methods=['GET'])
+    @app.route("/books", methods=["GET"])
     def get_books():
         connection = get_flask_database_connection(app)
         repository = BookRepository(connection)
         books = repository.all()
 
-        return render_template('books/index.html', books=books)
+        return render_template("books/index.html", books=books)
 
-    @app.route('/api/books', methods=['GET'])
+    @app.route("/api/books", methods=["GET"])
     def get_boots_api():
         connection = get_flask_database_connection(app)
         repository = BookRepository(connection)
@@ -25,39 +26,42 @@ def apply_example_routes(app):
 
     # GET /books/<id>
     # Returns a single book
-    @app.route('/books/<int:id>', methods=['GET'])
+    @app.route("/books/<int:id>", methods=["GET"])
     def get_book(id):
         connection = get_flask_database_connection(app)
         repository = BookRepository(connection)
         book = repository.find(id)
-        return render_template('books/show.html', book=book)
-
+        return render_template("books/show.html", book=book)
 
     # GET /books/new
     # Returns a form to create a new book
-    @app.route('/books/new', methods=['GET'])
+    @app.route("/books/new", methods=["GET"])
     def get_new_book():
-        return render_template('books/new.html')
-
+        return render_template("books/new.html")
 
     # POST /books
     # Creates a new book
-    @app.route('/books', methods=['POST'])
+    @app.route("/books", methods=["POST"])
     def create_book():
         # Set up the database connection and repository
         connection = get_flask_database_connection(app)
         repository = BookRepository(connection)
 
         # Get the fields from the request form
-        title = request.form['title']
-        author_name = request.form['author_name']
+        title = request.form["title"]
+        author_name = request.form["author_name"]
 
         # Create a book object
         book = Book(None, title, author_name)
 
         # Check for validity and if not valid, show the form again with errors
         if not book.is_valid():
-            return render_template('books/new.html', book=book, errors=book.generate_errors()), 400
+            return (
+                render_template(
+                    "books/new.html", book=book, errors=book.generate_errors()
+                ),
+                400,
+            )
 
         # Save the book to the database
         book = repository.create(book)
@@ -65,10 +69,9 @@ def apply_example_routes(app):
         # Redirect to the book's show route to the user can see it
         return redirect(f"/books/{book.id}")
 
-
     # POST /books/<id>/delete
     # Deletes a book
-    @app.route('/books/<int:id>/delete', methods=['POST'])
+    @app.route("/books/<int:id>/delete", methods=["POST"])
     def delete_book(id):
         connection = get_flask_database_connection(app)
         repository = BookRepository(connection)
@@ -78,5 +81,4 @@ def apply_example_routes(app):
         # rather than hand-writing them. `url_for` takes the name of the function
         # that generates the route and any arguments that are needed to generate
         # the URL.
-        return redirect(url_for('get_books'))
-
+        return redirect(url_for("get_books"))
